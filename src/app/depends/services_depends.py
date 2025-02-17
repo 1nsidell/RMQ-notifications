7f7b4b72137b680007d2ebd4e83391043c24280e
Daniel <1nsidell.cold@gmail.com>
2025-02-17 07:43:04 +0500

    refactor: use fast api depends for create di

--- src/app/depends/services_depends.py
+++ src/app/depends/services_depends.py
@@ -1,12 +1,17 @@
+from typing import Annotated
+
+from fastapi import Depends
 from fastapi_mail import FastMail
 
 from src.app.services.email_service import EmailServicesImpl, EmailServicesProtocol
 from src.settings import settings, Settings
 
 
-def get_email_service(settings: Settings) -> EmailServicesProtocol:
+def get_email_service(
+    settings: Settings = Depends(lambda: settings),
+) -> EmailServicesProtocol:
     mailer = FastMail(settings.fast_mail.conf)
     return EmailServicesImpl(mailer, settings)
 
 
-EmailService: EmailServicesProtocol = get_email_service(settings)
+EmailService = Annotated[EmailServicesProtocol, Depends(get_email_service)]
--- src/app/depends/use_cases_depends.py
+++ src/app/depends/use_cases_depends.py
@@ -1,9 +1,13 @@
+from typing import Annotated
+
+from fastapi import Depends
+
 from src.app.use_cases.send_email_use_case import EmailUseCaseImpl, EmailUseCaseProtocol
-from src.app.depends.services_depends import EmailServicesProtocol, EmailService
+from src.app.depends.services_depends import EmailService
 
 
-def get_email_use_case(email_service: EmailServicesProtocol) -> EmailUseCaseProtocol:
+def get_email_use_case(email_service: EmailService) -> EmailUseCaseProtocol:
     return EmailUseCaseImpl(email_service)
 
 
-EmailUseCase: EmailUseCaseProtocol = get_email_use_case(EmailService)
+EmailUseCase = Annotated[EmailUseCaseProtocol, Depends(get_email_use_case)]
